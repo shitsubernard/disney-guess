a3c259a85ca8595cd9cb8b2e8aaf27644890921b
shitsubernard <saga.kanon.gemeos@hotmail.com>
2025-10-18 05:31:55 -0300

    Update characters.js

--- characters.js
+++ characters.js
@@ -19,7 +19,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1928,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/6/67/Minnie_Mouse.png/revision/latest?cb=20240611033558"
+    image: "minnei.jpg"
   },
   {
     name: "Pato Donald",
@@ -30,7 +30,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1934,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/a/ab/Donald_Duck_Disney_1.png/revision/latest?cb=20180828172632"
+    image: "donald.jpg"
   },
   {
     name: "Pateta",
@@ -41,7 +41,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1932,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/e/e5/Goofy.png/revision/latest?cb=20250827192413"
+    image: "pateta.png"
   },
   {
     name: "Simba",
@@ -52,7 +52,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1994,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/3/37/Profile_-_Simba.jpeg/revision/latest/scale-to-width-down/1000?cb=20190312043852"
+    image: "simba.png"
   },
   {
     name: "Mufasa",
@@ -63,7 +63,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1994,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/5/5e/Mufasa_Lion_King_.jpg/revision/latest/scale-to-width-down/1000?cb=20240910202509"
+    image: "mufasa.jpg"
   },
   {
     name: "Scar",
@@ -74,7 +74,7 @@ const characters = [
     isVillain: true,
     firstAppearance: 1994,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/6/66/Profile_-_Scar.jpeg/revision/latest/scale-to-width-down/1000?cb=20250721194731"
+    image: "scar.jpg"
   },
   {
     name: "Ariel",
@@ -85,7 +85,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1989,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/8/8a/Profile_-_Ariel.jpg/revision/latest?cb=20230913063128"
+    image: "ariel.jpg"
   },
   {
     name: "Úrsula",
@@ -96,7 +96,7 @@ const characters = [
     isVillain: true,
     firstAppearance: 1989,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/7/72/Profile_-_Ursula.jpeg/revision/latest?cb=20201204230949"
+    image: "ursula.jpeg"
   },
   {
     name: "Elsa",
@@ -107,7 +107,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 2013,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/disney/images/9/95/Profile_-_Elsa.jpeg/revision/latest?cb=20241222112925"
+    image: "elsa.jpg"
   },
   {
     name: "Anna",
@@ -118,7 +118,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 2013,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/dis/images/7/7c/B5da8e4c0046a83b81dbd945719f6b354edd764b.jpeg/revision/latest?cb=20160623191458"
+    image: "anna.png"
   },
   {
     name: "Olaf",
@@ -129,7 +129,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 2013,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/5/53/Profile_-_Olaf.jpeg/revision/latest?cb=20200221075027"
+    image: "olaf.png"
   },
   {
     name: "Moana",
@@ -140,7 +140,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 2016,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/disney/images/7/7d/Profile_-_Moana.png/revision/latest/scale-to-width-down/1000?cb=20240530203852"
+    image: "moana.jpg"
   },
   {
     name: "Maui",
@@ -151,7 +151,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 2016,
     isHuman: false,
-    image: "https://static.wikia.nocookie.net/disney/images/6/6f/Profile_-_Maui.jpeg/revision/latest?cb=20250309150414"
+    image: "maui.jpg"
   },
   {
     name: "Mirabel Madrigal",
@@ -162,7 +162,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 2021,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/disney/images/2/2e/Profile_-_Mirabel_Madrigal.png/revision/latest/scale-to-width-down/1000?cb=20220213075457"
+    image: "mirabel.jpeg"
   },
   {
     name: "Isabela Madrigal",
@@ -173,7 +173,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 2021,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/disney/images/4/47/Profile_-_Isabela_Madrigal.png/revision/latest?cb=20211028173500"
+    image: "isabela.jpg"
   },
   {
     name: "Bruno Madrigal",
@@ -195,7 +195,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1999,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/disney/images/2/2e/Profile_-_Tarzan.png/revision/latest?cb=20190821020257"
+    image: "tarzan.jpg"
   },
   {
     name: "Jane Porter",
@@ -206,7 +206,7 @@ const characters = [
     isVillain: false,
     firstAppearance: 1999,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/disney/images/e/ef/Profile_-_Jane_Porter.png/revision/latest?cb=20190312070932"
+    image: "jane.jpg"
   },
   {
     name: "Clayton",
@@ -217,9 +217,10 @@ const characters = [
     isVillain: true,
     firstAppearance: 1999,
     isHuman: true,
-    image: "https://static.wikia.nocookie.net/disney/images/e/e2/Clayton_profile_.jpg/revision/latest?cb=20160325111414"
+    image: "clayton.jpg"
   }
 ];
+
 
 
 
